8de782e52af7ea64a135ab5b206c59ef623ae9af
Steve Quirke <steve@quirke.org>
2018-10-26 17:30:02 +0100

    Update signed_url.py

--- scripts/signed_url.py
+++ scripts/signed_url.py
@@ -10,9 +10,13 @@ print(build_dir)
 working_directory = build_dir + "/installapplications"
 output_directory = build_dir + "/installapplications/payload/Library/LaunchDaemons/"
 
-templateLoader = jinja2.FileSystemLoader(searchpath=working_directory)
-templateEnv = jinja2.Environment(loader=templateLoader)
-template = templateEnv.get_template("com.erikng.installapplications.template")
+env = Environment(loader=FileSystemLoader(working_directory))
+template = env.get_template(com.erikng.installapplications.template")
+
+
+#templateLoader = jinja2.FileSystemLoader(searchpath=working_directory)
+#templateEnv = jinja2.Environment(loader=templateLoader)
+#template = templateEnv.get_template("com.erikng.installapplications.template")
 filename = output_directory + "com.erikng.installapplications.plist"
 
 outputfile = open(filename, 'w')
